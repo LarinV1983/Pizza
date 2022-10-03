73a2662192f2f5f41645813595c485e163a1cb16
Viktor Larin <Larinv1983@mail.ru>
2022-10-03 09:13:35 +0300

    store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,12 @@
-import { createStore} from 'redux';
+import { createStore } from 'redux';
 import  {  composeWithDevTools  }  from  'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
 const store = createStore (rootReducer,
-	composeWithDevTools ( 
-    // applyMiddleware ( ... middleware ) 
-		)
+	composeWithDevTools ()
 	);
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>
 
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+export default store;
